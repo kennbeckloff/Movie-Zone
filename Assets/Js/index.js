928082ab7ca64b148db2f28612a7462ddcdb9939
kennbeckloff <kennbeckloff688@gmail.com>
2022-06-23 18:05:54 +0300

    linking api from the TMDH

---
+++ Assets/Js/index.js
@@ -0,0 +1,4 @@
+//getting data from the TMDH Api
+const base_url = "https://api.themoviedb.org/3"
+const api_key = "api_key=864abc344e6193436694c57936f427a3"
+const dicovery = "/discover/movie?sort_by=popularity.desc&"
